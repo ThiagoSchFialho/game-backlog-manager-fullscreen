2b83381fc24ac2cb0aa0b3dc51583c6adffbf52b
Thiago Scheffer Fialho <thiago.sch.fialho@gmail.com>
2026-09-06 13:33:41 -0300

    assets added

--- client/src/utils/getGameCover.ts
+++ client/src/utils/getGameCover.ts
@@ -17,5 +17,5 @@ export function slugifyGameName(name: string): string {
 export function getGameCover(name: string, format: string): string {
     const slug = slugifyGameName(name);
     const path = `../assets/games/${slug}/${format}.jpg`;
-    return gameCovers[path] ?? gameCovers['../assets/games/placeholder/square.jpg'];
+    return gameCovers[path] ?? gameCovers[`../assets/games/placeholder/${format}.jpg`];
 }
