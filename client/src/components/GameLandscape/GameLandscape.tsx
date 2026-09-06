dd17420f1ae6fd4ce112aecac591e8afc1cad14c
Thiago Scheffer Fialho <thiago.sch.fialho@gmail.com>
2026-09-05 22:43:09 -0300

    gameLandscape

---
+++ client/src/components/GameLandscape/GameLandscape.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import './styles.css';
+import { useDb } from '../../hooks/useDb';
+import type { Game } from '../../types/gamesType';
+import { orderBy } from '../../utils/orderBy';
+import play from '../../assets/icons/play.svg';
+
+interface GameCardsProps {
+    id: string;
+    steamId: string;
+    img: string;
+    name: string;
+}
+
+const GameLandscape: React.FC<GameCardsProps> = ({ id, steamId, img, name }) => {
+    const { fetchGames, updateStatus } = useDb();
+    
+    const getGames = async () => {
+        const games = await fetchGames();
+        if (!games) {
+            alert("Erro ao recuperar jogos.");
+        }
+        return games;
+    }
+
+    const changeStatus = async (id: string, status: string) => {
+        const MAX_PLAYING_GAMES = 5;
+
+        if (status === "playing") {
+            const games = await getGames();
+            const playingGames = games.filter((game: Game) => game.status === "playing");
+            const orderedPlayingGames = orderBy(playingGames, "rtime_last_played", "asc");
+
+            if (orderedPlayingGames.length >= MAX_PLAYING_GAMES) {
+                const oldestGame = orderedPlayingGames[0];
+                const freedSlot = await updateStatus(oldestGame.id, "played");
+                if (!freedSlot) return;
+            }
+        }
+
+        const updated = await updateStatus(id, status);
+        if (!updated) return;
+    };
+
+    const handleStartGame = async (id: string, steamId: string) => {
+        window.location.href = `steam://rungameid/${steamId}`;
+        changeStatus(id, "playing");
+    }
+
+    return (
+        <div onClick={() => handleStartGame(id, steamId)} className="game-landscape">
+            <img
+                className="game-landscape-img"
+                src={img}
+                alt={name}
+            />
+            <div className="play-btn-landscape">
+                <img src={play} alt="play" />
+                <p>Jogar</p>
+            </div>
+        </div>
+    );
+};
+
+export default GameLandscape;
