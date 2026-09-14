6aff8762d6591d6e36b0acc181fad93aca411690
Thiago Scheffer Fialho <thiago.sch.fialho@gmail.com>
2026-09-13 22:25:53 -0300

    navigation with gamepad on gamepage

--- client/src/pages/GamePage/GamePage.tsx
+++ client/src/pages/GamePage/GamePage.tsx
@@ -1,27 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import './styles.css';
-import SideMenu from '../../components/SideMenu/SideMenu';
-import { getGameCover } from '../../utils/getGameCover';
-import { useDb } from '../../hooks/useDb';
-import type { Game } from '../../types/gamesType';
-import GameLandscape from '../../components/GameLandscape/GameLandscape';
 import { useNavigate, useParams } from 'react-router-dom';
+import './styles.css';
+
+import SideMenu from '../../components/SideMenu/SideMenu';
+import GameLandscape from '../../components/GameLandscape/GameLandscape';
+import JoystickSetup from '../../components/JoystickSetup/JoystickSetup';
+
+import { useDb } from '../../hooks/useDb';
+
+import { getGameCover } from '../../utils/getGameCover';
+
 import playing from '../../assets/icons/playing.svg';
 import played from '../../assets/icons/played.svg';
 import notPlayed from '../../assets/icons/not-played.svg';
 import completed from '../../assets/icons/completed.svg';
 import totalTime from '../../assets/icons/total-time.svg';
 import trophy from '../../assets/icons/trophy.svg';
-import JoystickSetup from '../../components/JoystickSetup/JoystickSetup';
-import { orderBy } from '../../utils/orderBy';
 
-type ScreenItem = {
-    id: Game['id'];
-    steamId: Game['steam_id'];
-    img: string;
-    name: Game['title'];
-    action: () => void | Promise<void>;
-};
+import type { Game } from '../Collection/Collection';
+import { useSound } from '../../hooks/useSound';
+interface ScreenItems {
+    label: string
+    action:  () => void | Promise<void>
+}
 
 const statusConfig = {
     playing: { icon: playing, label: 'Jogando', color: '#1FC06D' },
@@ -30,14 +31,17 @@ const statusConfig = {
     completed: { icon: completed, label: 'Zerado', color: '#7B5CFF' },
 };
 
+const BANNER_INDEX = 999;
+
+
 const GamePage: React.FC = () => {
     const { id } = useParams<{ id: string }>();
     const navigation = useNavigate();
-    const { fetchGames, updateStatus } = useDb();
-    const { getGameById } = useDb();
+    const { getGameById, handleStartGame } = useDb();
+    const { playSelectSound, playConfirmSound } = useSound();
     const [currentGame, setCurrentGame] = useState<Game>();
     const currentStatus = statusConfig[currentGame?.status ?? 'not-played'];
-    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [selectedIndex, setSelectedIndex] = useState(999);
     
     useEffect(() => {
         const getGame = async () => {
@@ -49,48 +53,70 @@ const GamePage: React.FC = () => {
         getGame();
     }, []);
 
-    const changeStatus = async (id: string, status: string) => {
-        const MAX_PLAYING_GAMES = 5;
-
-        if (status === "playing") {
-            const games = (await fetchGames()) as Game[] | undefined;
-            if (!games) return;
-
-            const playingGames = games.filter((game: Game) => game.status === "playing");
-            const orderedPlayingGames = orderBy(playingGames, "rtime_last_played", "asc");
-
-            if (orderedPlayingGames.length >= MAX_PLAYING_GAMES) {
-                const oldestGame = orderedPlayingGames[0];
-                const freedSlot = await updateStatus(oldestGame.id, "played");
-                if (!freedSlot) return;
-            }
-        }
-
-        const updated = await updateStatus(id, status);
-        if (!updated) return;
-    };
-
-    const handleStartGame = async (id: string, steamId: string) => {
-        window.location.href = `steam://rungameid/${steamId}`;
-        changeStatus(id, "playing");
-    }
-
-    const screenItems: ScreenItem[] = currentGame ? [
+    const screenItems: ScreenItems[] = [
         {
-            id: currentGame.id,
-            steamId: currentGame.steam_id,
-            img: getGameCover(currentGame.title, 'landscape'),
-            name: currentGame.title,
-            action: () => handleStartGame(currentGame.id, currentGame.steam_id)
+            label: 'Zeravel:',
+            action: () => console.log('action not implemented')
+        },
+        {
+            label: 'Alterar status',
+            action: () => console.log('action not implemented')
+        },
+        {
+            label: 'Adicionar à coleção',
+            action: () => console.log('action not implemented')
+        },
+        {
+            label: 'Remover da coleção',
+            action: () => console.log('action not implemented')
+        },
+        {
+            label: 'Ocultar jogo',
+            action: () => console.log('action not implemented')
         }
-    ] : [];
+    ];
 
     const joystickNavigation = (command: string) => {
-        if (command === 'A') {
-            screenItems[selectedIndex]?.action();
-        }
-        if (command === 'B') {
-            navigation(-1);
+        if (selectedIndex === BANNER_INDEX) {
+            if (command === 'baixo') {
+                playSelectSound();
+                setSelectedIndex(0);
+            } else if (command === 'A') {
+                if (currentGame) {
+                    handleStartGame(currentGame.id, currentGame.steam_id)
+                }
+            }
+        } else {
+            if (command === 'esquerda') {
+                if (selectedIndex % 2 !== 0) {
+                    playSelectSound();
+                    setSelectedIndex(selectedIndex - 1);
+                }
+            } else if (command === 'direita') {
+                if (selectedIndex % 2 === 0) {
+                    if (selectedIndex < screenItems.length - 1) {
+                        playSelectSound();
+                        setSelectedIndex(selectedIndex + 1);
+                    }
+                }
+            } else if (command === 'cima') {
+                playSelectSound();
+                if (selectedIndex < 2) {
+                    setSelectedIndex(999);
+                } else {
+                    setSelectedIndex(selectedIndex - 2);
+                }
+            } else if (command === 'baixo') {
+                if (selectedIndex < screenItems.length - 2 ){
+                    playSelectSound();
+                    setSelectedIndex(selectedIndex + 2);
+                }
+            } else if (command === 'A') {
+                playConfirmSound();
+                screenItems[selectedIndex].action();
+            } else if (command === 'B') {
+                navigation(-1);
+            }
         }
     };
 
@@ -104,27 +130,32 @@ const GamePage: React.FC = () => {
                 ): (
                     <>
                         <h1 className="game-page-title">{currentGame?.title}</h1>
-                        {screenItems.slice(0, 1).map(item => (
                             <GameLandscape
-                                id={item.id}
-                                steamId={item.steamId}
-                                img={item.img}
-                                name={item.name}
-                                isFocused={selectedIndex === 0}
+                                id={currentGame.steam_id}
+                                steamId={currentGame.steam_id}
+                                img={getGameCover(currentGame.title, 'landscape')}
+                                name={currentGame.title}
+                                isFocused={selectedIndex === 999}
                             />
-                        ))}
 
                         <div className="game-page-details-container">
                             <div className="game-page-details-section">
                                 <h2 className="game-page-details-section-title">Opções</h2>
-                                <div className="game-page-details-item game-page-options-item">
-                                    <p>Alterar status</p>
-                                </div>
-                                <div className="game-page-details-item game-page-options-item">
-                                    <p>Adicionar à coleção</p>
-                                </div>
-                                <div className="game-page-details-item game-page-options-item">
-                                    <p>Ocultar jogo</p>
+                                <div className="game-page-options-container">
+                                    {screenItems.map((item, index) => (
+                                        <div
+                                            className={ selectedIndex === index ? "game-page-details-item game-page-options-item options-item-focused" : "game-page-details-item game-page-options-item"}
+                                        >
+                                            {index === 0 ? (
+                                                <>
+                                                    <p>{item.label}</p>
+                                                    <p>sim</p>
+                                                </>
+                                            ) : (
+                                                <p>{item.label}</p>
+                                            )}
+                                        </div>
+                                    ))}
                                 </div>
                             </div>
 
