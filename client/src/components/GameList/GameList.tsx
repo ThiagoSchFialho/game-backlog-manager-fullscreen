6cb89451bce6431da46c95322f31dfd6435644b0
Thiago Scheffer Fialho <thiago.sch.fialho@gmail.com>
2026-09-16 20:24:23 -0300

    GameList component

---
+++ client/src/components/GameList/GameList.tsx
@@ -0,0 +1,210 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './styles.css';
+
+import GameCard from '../GameCard/GameCard';
+import JoystickSetup from '../JoystickSetup/JoystickSetup';
+import GamePage from '../GamePage/GamePage';
+
+import { useDb } from '../../hooks/useDb';
+import { useSound } from '../../hooks/useSound';
+import { useScroll } from '../../hooks/useScroll';
+
+import { getGameCover } from '../../utils/getGameCover';
+import { orderBy } from '../../utils/orderBy';
+
+import recentlyPlayed from '../../assets/icons/recently-played.svg';
+import mostPlayed from '../../assets/icons/most-played.svg'
+import alphabet from '../../assets/icons/alphabet.svg';
+
+import type { Game } from '../../types/gamesType';
+type ScreenItem = {
+    id: Game['id'];
+    steamId: Game['steam_id'];
+    img: string;
+    name: Game['title'];
+    action: () => void | Promise<void>;
+};
+
+interface GameListProps {
+    list: Game[]
+    onReloadList: () => void,
+    sortingMethod: string | undefined,
+}
+
+
+const GameList: React.FC<GameListProps> = ({ list, onReloadList, sortingMethod }) => {
+    const { playSelectSound, playConfirmSound, playPopupSound, playSwipeSound } = useSound();
+    const [sortMethod, setSortMethod] = useState(sortingMethod ?? 'alphabet');
+    const [gamesList, setGamesList] = useState<Game[]>(list);
+    const [sortedGamesList, setSortedGamesList] = useState<Game[]>([]);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [commandCoolDown, setCommandCoolDown] = useState(false);
+    const [isOnGamePage, setIsOnGamePage] = useState(false);
+    const [selectedGameId, setSelectedGameId] = useState<Game['id'] | undefined>(undefined);
+
+    useEffect(() => {
+        setGamesList(list);
+    }, [onReloadList]);
+
+    const sortGames = (method: string, list: Game[] = gamesList) => {
+        setSortMethod(method);
+
+        if (method === 'alphabet') {
+            setSortedGamesList(orderBy(list, 'title', 'asc'));
+        } else if (method === 'mostPlayed') {
+            setSortedGamesList(orderBy(list, 'playtime', 'desc'));
+        } else if (method === 'recentlyPlayed') {
+            setSortedGamesList(orderBy(list, 'rtime_last_played', 'desc'));
+        }        
+    }
+
+    useEffect(() => {
+        const sortMethodAux = sortMethod;
+        const selectedIndexAux = selectedIndex;
+
+        onReloadList();
+        setSortMethod(sortMethodAux);
+        setSelectedIndex(selectedIndexAux);
+    }, [isOnGamePage]);
+
+    useEffect(() => {
+        sortGames(sortingMethod ?? 'alphabet', gamesList);
+    }, [sortingMethod]);
+
+    useEffect(() => {
+        sortGames(sortMethod, gamesList);
+    }, [gamesList]);
+    
+    const gameCardsItems: ScreenItem[] = sortedGamesList.map((game) => ({
+        id: game.id,
+        steamId: game.steam_id,
+        img: getGameCover(game.title, 'square'),
+        name: game.title,
+        action: () => { setSelectedGameId(game.id); setIsOnGamePage(true) }
+    }));
+    const { scrollContainerRef, setCardRef } = useScroll(selectedIndex, gameCardsItems.length);
+
+    // --- Refs para evitar stale closure no joystickNavigation -------------
+    const selectedIndexRef = useRef(selectedIndex);
+    useEffect(() => {
+        selectedIndexRef.current = selectedIndex;
+    }, [selectedIndex]);
+
+    const itemsLengthRef = useRef(gameCardsItems.length);
+    useEffect(() => {
+        itemsLengthRef.current = gameCardsItems.length;
+    }, [gameCardsItems.length]);
+    // ------------------------------------------------------------------
+
+    const sortGamesMethods = ['alphabet', 'mostPlayed', 'recentlyPlayed'];
+    const joystickNavigation = (command: string) => {
+        const currentIndex = selectedIndexRef.current;
+        const length = itemsLengthRef.current;
+
+        if (command === 'esquerda') {
+            if (currentIndex !== 0) {
+                playSelectSound();
+                setSelectedIndex(currentIndex - 1);
+            }
+        } else if (command === 'direita') {
+            if (currentIndex !== length - 1) {
+                playSelectSound();
+                setSelectedIndex(currentIndex + 1);
+            }
+        } else if (command === 'cima') {
+            if (currentIndex > 4) {
+                playSelectSound();
+                setSelectedIndex(currentIndex - 5);
+            }
+        } else if (command === 'baixo') {
+            if (currentIndex < length - 5) {
+                playSelectSound();
+                setSelectedIndex(currentIndex + 5);
+            }
+        } else if (command === 'A') {
+            if (commandCoolDown) return null;
+            playConfirmSound();
+            gameCardsItems[currentIndex]?.action();
+        } else if (command === 'Y') {
+            playPopupSound();
+            setIsMenuOpen(true);
+        } else if (command === 'RB') {
+            if (sortMethod !== sortGamesMethods[sortGamesMethods.length - 1]) {
+                playSwipeSound();
+                const currentIndex = sortGamesMethods.indexOf(sortMethod);
+                setSortMethod(sortGamesMethods[currentIndex + 1]);
+                sortGames(sortGamesMethods[currentIndex + 1], gamesList);
+                setSelectedIndex(0);
+            }
+        } else if (command === 'LB') {
+            if (sortMethod !== sortGamesMethods[0]) {
+                playSwipeSound();
+                const currentIndex = sortGamesMethods.indexOf(sortMethod);
+                setSortMethod(sortGamesMethods[currentIndex - 1]);
+                sortGames(sortGamesMethods[currentIndex - 1], gamesList);
+                setSelectedIndex(0);
+            }
+        }
+    };
+
+    const handleCloseMenu = () => {
+        setCommandCoolDown(true);
+        setTimeout(() => {
+            setCommandCoolDown(false);
+        }, 50);
+        setIsMenuOpen(false);
+    }
+    
+    return (
+        <>
+            {isOnGamePage && selectedGameId !== undefined && (
+                <GamePage gameId={selectedGameId} onExitGamePage={() => setIsOnGamePage(false)} />
+            )}
+
+            {!isMenuOpen && !isOnGamePage && <JoystickSetup command={joystickNavigation} />}
+            <div className="main-content" style={{ display: isOnGamePage ? 'none' : undefined }}>
+                <div className="sortings-container">
+                    <ul>
+                        <li
+                            className={sortMethod === 'alphabet' ? 'selected-method' : ''}
+                        >
+                            <img src={alphabet} />
+                            <p>Alfabeticamente</p>
+                        </li>
+                        <li
+                            className={sortMethod === 'mostPlayed' ? 'selected-method' : ''}
+                        >
+                            <img src={mostPlayed} />
+                            <p>Mais jogados</p>
+                        </li>
+                        <li
+                            className={sortMethod === 'recentlyPlayed' ? 'selected-method' : ''}
+                        >
+                            <img src={recentlyPlayed} />
+                            <p>Jogados Recentemente</p>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="list-game-container" ref={scrollContainerRef}>
+                    {gameCardsItems.map((item, index) => (
+                        <div key={item.id} ref={setCardRef(index)}>
+                            <GameCard
+                                id={item.id}
+                                steamId={item.steamId}
+                                img={item.img}
+                                name={item.name}
+                                isFocused={selectedIndex === index}
+                                isOpen={isMenuOpen && selectedIndex === index}
+                                onCloseMenu={handleCloseMenu}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default GameList;
