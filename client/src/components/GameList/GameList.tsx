049719056d6e023567d5eec6d9c98c25f2c9f95b
Thiago Scheffer Fialho <thiago.sch.fialho@gmail.com>
2026-09-24 23:18:59 -0300

    fix: scroll on GameList

--- client/src/components/GameList/GameList.tsx
+++ client/src/components/GameList/GameList.tsx
@@ -214,55 +214,57 @@ const GameList: React.FC<GameListProps> = ({ list, onReloadList, sortingMethod, 
 
             {!isMenuOpen && !isOnGamePage && <JoystickSetup command={joystickNavigation} />}
             <div className="main-content" style={{ display: isOnGamePage ? 'none' : undefined }}>
-                <div className="sortings-container">
-                    <h1>{title ?? ''}</h1>
-                    <ul>
-                        <li
-                            className={sortMethod === 'alphabet' ? 'selected-method' : ''}
-                        >
-                            <img src={alphabet} />
-                            <p>Alfabeticamente</p>
-                        </li>
-                        <li
-                            className={sortMethod === 'mostPlayed' ? 'selected-method' : ''}
-                        >
-                            <img src={mostPlayed} />
-                            <p>Mais jogados</p>
-                        </li>
-                        <li
-                            className={sortMethod === 'recentlyPlayed' ? 'selected-method' : ''}
-                        >
-                            <img src={recentlyPlayed} />
-                            <p>Jogados Recentemente</p>
-                        </li>
-                    </ul>
-                </div>
+                <div className="game-list-container">
+                    <div className="sortings-container">
+                        <h1>{title ?? ''}</h1>
+                        <ul>
+                            <li
+                                className={sortMethod === 'alphabet' ? 'selected-method' : ''}
+                            >
+                                <img src={alphabet} />
+                                <p>Alfabeticamente</p>
+                            </li>
+                            <li
+                                className={sortMethod === 'mostPlayed' ? 'selected-method' : ''}
+                            >
+                                <img src={mostPlayed} />
+                                <p>Mais jogados</p>
+                            </li>
+                            <li
+                                className={sortMethod === 'recentlyPlayed' ? 'selected-method' : ''}
+                            >
+                                <img src={recentlyPlayed} />
+                                <p>Jogados Recentemente</p>
+                            </li>
+                        </ul>
+                    </div>
 
-                <div className="list-game-container" ref={scrollContainerRef}>
-                    {gameCardsItems.length === 0 ? (
-                        <div className="message-container">
-                            <h3>Nenhum jogo aqui.</h3>
-                        </div>
-                    ) : (
-                        gameCardsItems.map((item, index) => (
-                            <React.Fragment key={item.id}>
-                                {index === firstNotInstalledIndex && index !== 0 && (
-                                    <hr className="installed-divider" />
-                                )}
-                                <div ref={setCardRef(index)} onClick={() => {setSelectedGameId(item.id); setIsOnGamePage(true)}}>
-                                    <GameCard
-                                        id={item.id}
-                                        steamId={item.steamId}
-                                        img={item.img}
-                                        name={item.name}
-                                        isFocused={selectedIndex === index}
-                                        isOpen={isMenuOpen && selectedIndex === index}
-                                        onCloseMenu={handleCloseMenu}
-                                    />
-                                </div>
-                            </React.Fragment>
-                        ))
-                    )}
+                    <div className="list-game-container" ref={scrollContainerRef}>
+                        {gameCardsItems.length === 0 ? (
+                            <div className="message-container">
+                                <h3>Nenhum jogo aqui.</h3>
+                            </div>
+                        ) : (
+                            gameCardsItems.map((item, index) => (
+                                <React.Fragment key={item.id}>
+                                    {index === firstNotInstalledIndex && index !== 0 && (
+                                        <hr className="installed-divider" />
+                                    )}
+                                    <div ref={setCardRef(index)} onClick={() => {setSelectedGameId(item.id); setIsOnGamePage(true)}}>
+                                        <GameCard
+                                            id={item.id}
+                                            steamId={item.steamId}
+                                            img={item.img}
+                                            name={item.name}
+                                            isFocused={selectedIndex === index}
+                                            isOpen={isMenuOpen && selectedIndex === index}
+                                            onCloseMenu={handleCloseMenu}
+                                        />
+                                    </div>
+                                </React.Fragment>
+                            ))
+                        )}
+                    </div>
                 </div>
             </div>
         </>
